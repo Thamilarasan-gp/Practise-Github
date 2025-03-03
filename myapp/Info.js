0768d8199488cfb89ba3bebde6cdc40e4a42c2e8
iamrahulr2023 <iamrahul.r.2005@gmail.com>
2025-03-03 12:10:27 +0530

    rk info

---
+++ myapp/Info.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Info = () => {
+  return (
+   <View>
+     <Text> This is the Info screen</Text>  
+    </View>
+  )
+}
+
+export default Info
